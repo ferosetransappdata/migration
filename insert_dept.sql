67557d01dc45ade3affd70a0261bfb7f793b2898
raghutransappdata <s.raghunath@transappdata.com>
2019-01-17 13:11:50 +0530

    deleted the old sample files

--- insert_dept.sql
+++
@@ -1,4 +0,0 @@
-Insert into DEPT (DEPTNO,DNAME,LOC) values (10,'ACCOUNTING','NEW YORK');
-Insert into DEPT (DEPTNO,DNAME,LOC) values (20,'RESEARCH','DALLAS');
-Insert into DEPT (DEPTNO,DNAME,LOC) values (30,'SALES','CHICAGO');
-Insert into DEPT (DEPTNO,DNAME,LOC) values (40,'OPERATIONS','BOSTON');
