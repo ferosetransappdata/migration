3d45bf811a8212d17336110f95d000c4d6ad0be2
ferosetransappdata <44697695+ferosetransappdata@users.noreply.github.com>
2019-01-17 10:40:41 +0530

    version 2.0
    this is the first commit for insert_dept.sql

---
+++ insert_dept.sql
@@ -0,0 +1,4 @@
+Insert into DEPT (DEPTNO,DNAME,LOC) values (10,'ACCOUNTING','NEW YORK');
+Insert into DEPT (DEPTNO,DNAME,LOC) values (20,'RESEARCH','DALLAS');
+Insert into DEPT (DEPTNO,DNAME,LOC) values (30,'SALES','CHICAGO');
+Insert into DEPT (DEPTNO,DNAME,LOC) values (40,'OPERATIONS','BOSTON');
